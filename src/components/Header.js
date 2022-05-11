984e860afc61d9dcb5f216beb98a6d35466bc2fd
Sam <noci0001@hz.nl>
2022-05-11 12:38:34 +0200

    stable build

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <header>
+            <h1>This is my header</h1>
+        </header>
+    </div>
+  )
+}
